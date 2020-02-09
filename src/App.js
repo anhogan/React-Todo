eb8072304bcf99f5f2458ea2ebe360f5e9bf7879
Amanda Hogan <ahogan@teamworks.com>
2020-02-09 18:44:33 -0500

    Create event handlers

--- src/App.js
+++ src/App.js
@@ -32,12 +32,57 @@ class App extends React.Component {
       newTask: ""
     };
   };
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  toggleComplete = (id) => {
+    const newState = {
+      ...this.state,
+      todoList: this.state.todoList.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed
+          };
+        }
+        return task;
+      })
+    };
+    this.setState(newState);
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.addNewItem(this.state.newTask);
+  };
+
+  handleChange = (event) => {
+    this.setState({ ...this.state, newTask: event.target.value });
+  };
+
+  addTodo = (newToDo) => {
+    const newState = {
+      ...this.state,
+      todoList: [
+        ...this.state.todoList,
+        { task: newToDo, completed: false, id: Date.now() }
+      ]
+    };
+    this.setState(newState);
+  };
+
+  clearCompleted = () => {
+    const newState = {
+      ...this.state,
+      todoList: this.state.todoList.filter(task => {
+        return !task.completed;
+      })
+    };
+    this.setState(newState);
+  };
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Welcome to your To-Do App!</h2>
         <ToDoList
           todos={this.state.todoList}
           toggleComplete={this.toggleComplete}
